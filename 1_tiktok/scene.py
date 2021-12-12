9a616e119a63144d628b3ef68b0cf65671fd022f
jennyzzt <jennylive158@gmail.com>
2021-12-12 21:10:48 +0800

    Added generation of user-item matrix

--- 1_tiktok/scene.py
+++ 1_tiktok/scene.py
@@ -15,14 +15,20 @@ class VideoScene(Scene):
         self.wait()
 
         self.next_section("user-item matrix", skip_animations=False)
-        self.show_matrix()
+        useritem_matrix = self.create_ui_matrix()
+        self.add(useritem_matrix.scale(0.7))
+        self.play(FadeIn(Text("User-Item Matrix").scale(0.7).next_to(useritem_matrix, UP, buff=0.5)))
+        self.wait(1)
 
-    def show_matrix(self):
+    def create_ui_matrix(self):
         matrix_size = (5, 4)
 
+        # create table with example values
         poss_vals = list(range(6))
-        poss_vals_prob = np.array([20] + [1]*5) / 25
-        dummy_vals = np.random.choice(poss_vals, matrix_size, p=poss_vals_prob)
+        # # if matrix is bigger and more entries are needed
+        # poss_vals_prob = np.array([20] + [1]*5) / 25
+        # dummy_vals = np.random.choice(poss_vals, matrix_size, p=poss_vals_prob)
+        dummy_vals = np.zeros(matrix_size)
         for vals in dummy_vals:
             # ensure that every row has at least one value
             vals[np.random.randint(len(vals))] = np.random.randint(1, 5)
@@ -30,11 +36,23 @@ class VideoScene(Scene):
             # ensure that every column has at least one value
             dummy_vals[np.random.randint(matrix_size[0])][i] = np.random.randint(1, 5)
         poss_objs = [(Text(str(i)) if i > 0 else Text(" ")) for i in poss_vals]
-        dummy_objs = [list(map(lambda v : poss_objs[v].copy(), vs)) for vs in dummy_vals]
-        
-        # TODO: extend the table with Mobject images at the left and top
+        dummy_objs = [list(map(lambda v : poss_objs[int(v)].copy(), vs)) for vs in dummy_vals]
+        table = MobjectTable(dummy_objs, include_outer_lines=False).scale(0.7)
 
-        table = MobjectTable(dummy_objs, include_outer_lines=False)
-        self.add(table)
-        self.wait(1)
-        self.remove(table)
+        # add row images
+        user_img = ImageMobject("./scene_images/user.png").scale(0.15)
+        row_img = user_img.copy()
+        for _ in range(matrix_size[0] - 1):
+            row_img.add(user_img.copy().next_to(row_img, DOWN, buff=0.4))
+        row_img.next_to(table, LEFT).align_to(table, UP)
+        # add column images
+        poss_imgs_fn = ["game", "dance", "cat", "tree"]
+        poss_imgs = [ImageMobject(f"./scene_images/{fn}.png").scale(0.25) for fn in poss_imgs_fn]
+        col_img = poss_imgs[0].copy()
+        for i in range(1, len(poss_imgs)):
+            col_img.add(poss_imgs[i].copy().next_to(col_img, RIGHT, buff=0.25))
+        col_img.next_to(table, UP).align_to(table, LEFT)
+
+        # group table and images together
+        useritem_matrix = Group(table, row_img, col_img)
+        return useritem_matrix
